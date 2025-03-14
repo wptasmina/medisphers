5687954e4c21501b5a7a3450218d2ad975c74b6e
wptasmina <wptasmina@gmail.com>
2025-03-14 15:31:40 +0600

    added hero sertion and top doctors cord

---
+++ src/app/_components/ServiceCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function ServiceCard({ item }) {
+    const { img, name, year, availability, datetime, desc } = item;
+  
+  return (
+    <div className=" overflow-hidden rounded-lg shadow-sm transition hover:shadow-lg">
+      <img
+        alt="image"
+        src={img}
+        className="h-56 w-full object-cover  object-right-top"
+      />
+
+      <div className="bg-white p-4 sm:p-6">
+        <div className='flex justify-between items-center '>
+          <div>
+            <time dateTime={datetime} className="block text-xs text-gray-500">{datetime}</time>
+          </div>
+          <p>{availability}</p>
+        </div>
+
+        <div className='flex justify-between items-center '>
+          <h3 className="mt-0.5 text-lg text-gray-900 font-bold">{name}</h3>
+          <p>Year: {year}</p>
+        </div>
+
+        <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+          {desc}
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 import { Button } from "@/components/ui/button"
+import Hero from "./_components/Hero";
+import Service from './_components/Service';
+
+
+
 
 
 export default function Home() {
   return (
-   <div className="text-6xl text-center">
-    Hello word
-    <h1>Welcome to Medisphers</h1>
+   <div className="">
+    <Hero/>
+    <Service/>
+    
+
    </div>
 
   );
