b2f90b36f5d14d458b89670731a1cbcd9be9f30d
anamul hoque <anamul.eee73@gmail.com>
2025-03-14 15:14:41 +0600

    added Meet Expertise

--- src/components/ui/DoctorSection.jsx
+++ src/components/ui/DoctorSection.jsx
@@ -105,7 +105,7 @@ const DoctorSection = () => {
   };
 
   return (
-    <section className="py-11 bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+    <section className="py-11 bg-gradient-to-r from-fuchsia-600 to-purple-600 text-white">
       <div className="container w-11/12 mx-auto text-center">
         <h2 className="text-4xl font-bold mb-4 tracking-wide">Meet Our Experts</h2>
         <p className="text-xl mb-6 opacity-80">
