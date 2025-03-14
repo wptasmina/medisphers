8334ad6da00354c2e8175b4dd489b4ef4705dcc4
anamul hoque <anamul.eee73@gmail.com>
2025-03-14 09:06:30 +0600

    addrd Experties Doctor section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button"
+import DoctorSection from "@/components/ui/DoctorSection";
 
 
 export default function Home() {
   return (
    <div className="text-6xl text-center">
     Hellow word
+    <DoctorSection></DoctorSection>
    </div>
 
   );
