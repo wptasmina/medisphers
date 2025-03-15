772da01d5620846d891080afad6134fd6f8e9397
wptasmina <wptasmina@gmail.com>
2025-03-15 15:35:19 +0600

    padding change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ export default function NavBar() {
     return (
         <div className="navbar bg-base-300 shadow-sm">
             <div className="w-11/12 mx-auto">
-                <div className="navbar-start">
+                <div className="navbar-start p-0">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
