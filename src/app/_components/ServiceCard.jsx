0508c00a69236a80390736d82eba0872732bcbf1
Joy Chowdhury <imjoychy69@gmail.com>
2025-03-17 01:00:00 +0600

    resolved all mistakes

--- src/app/_components/ServiceCard.jsx
+++ src/app/_components/ServiceCard.jsx
@@ -1,10 +1,10 @@
-
-import { Button } from '@/components/ui/button';
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 export default function ServiceCard({ item }) {
-    const { img, name, year, availability, datetime, desc } = item;
-  
+  const { img, name, year, availability, datetime, desc } = item;
+
   return (
     <div className=" overflow-hidden rounded-lg shadow-sm transition hover:shadow-lg">
       <img
@@ -14,14 +14,16 @@ export default function ServiceCard({ item }) {
       />
 
       <div className="bg-white p-4 sm:p-6">
-        <div className='flex justify-between items-center '>
+        <div className="flex justify-between items-center ">
           <div>
-            <time dateTime={datetime} className="block text-xs text-gray-500">{datetime}</time>
+            <time dateTime={datetime} className="block text-xs text-gray-500">
+              {datetime}
+            </time>
           </div>
           <p className="text-gray-900 font-medium">{availability}</p>
         </div>
 
-        <div className='flex justify-between items-center '>
+        <div className="flex justify-between items-center ">
           <h3 className="mt-0.5 text-lg text-gray-900 font-bold">{name}</h3>
           <p>Year: {year}</p>
         </div>
@@ -29,7 +31,9 @@ export default function ServiceCard({ item }) {
         <p className="mt-2 mb-4 line-clamp-3 text-sm/relaxed text-gray-500">
           {desc}
         </p>
-        <Button >Details</Button>
+        <Link href={'/doctor'}>
+          <Button>Details</Button>
+        </Link>
       </div>
     </div>
   );
