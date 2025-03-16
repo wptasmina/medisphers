4371213e861a30641333e643da9eefb3f80f10a9
wptasmina <wptasmina@gmail.com>
2025-03-17 04:31:20 +0600

    update main branch

---
+++ src/app/doctor/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+// import DoctorDetails from '@/components/doctorDetails/DoctorDetails';
+
+
+const page = () => {
+    return (
+        <div>
+            {/* <DoctorDetails /> */}
+        </div>
+    );
+};
+
+export default page;
