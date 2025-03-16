6007180541daab95f9d2d92006446f64793af400
wptasmina <wptasmina@gmail.com>
2025-03-17 03:27:58 +0600

    chage navbae logo, text button remove, added navbar active colour, or  added  find doctor search, change top doctor's button

--- src/app/_components/Hero.jsx
+++ src/app/_components/Hero.jsx
@@ -1,26 +1,31 @@
 import { Button } from '@/components/ui/button'
+import { ArrowUpRight, Icon } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Hero() {
   return (
     <section>
-      <div className="w-11/12 mx-auto max-w-screen-xl py-8 sm:px-6 ">
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:items-center self-start md:gap-8">
+      <div className="w-11/12 mx-auto  py-8 px-3 ">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 items-center self-start md:gap-8">
           <div>
-            <div className="max-w-lg md:max-w-none self-start">
-            <h2 className='pt-3 md:text-6xl text-4xl font-extrabold'> Your <span className='text-blue-600'>Health</span>, Our <span className='text-blue-600'>Priority</span> </h2>
-            <p className='py-4'>MediSheper helps hospitals perform their daily operations quickly, accurately, and efficiently with the help of advanced technology.</p>
-            <Button >Explorer</Button>
-
+            <div className="max-w-lg md:max-w-none self-start  px-3 md:px-0">
+            <h2 className='pt-3 lg:text-6xl text-4xl font-extrabold'> Your <span className='text-[#022dbb]'>Health</span>,<br/>Our <span className='text-[#022dbb]'>Priority</span></h2>
+            <p className='py-4 lg:w-[550px]'>MediSheper helps hospitals perform their daily operations quickly, accurately, and efficiently with the help of advanced technology.</p>
+            <Link href="/" >
+            <Button className="bg-[#022dbb] px-4 py-2 inline-flex cursor-pointer " >
+              Book appointment<ArrowUpRight />
+            </Button>
+            </Link>
           </div>
           </div>
 
           {/* ✅ Regular img tag for React */}
           <div>
             <img
-              src="/banner.jpg"
+              src="/banner.png"
               width={600}
-              height={600}
+              
               className="rounded"
               alt="Doctor image"
             />
