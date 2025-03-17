5ea65de867223aec3486b37215e188c3a83ad4f4
wptasmina <wptasmina@gmail.com>
2025-03-17 20:38:57 +0600

    added mongodb install and create folder

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,19 @@
+// # DB_NAME=medisphere
+// # DB_PUSS=lMAuuWszCYRCUwox
+
+
+import { MongoClient, ServerApiVersion } from'mongodb';
+
+export default function dbConnect(collectionName){
+    const uri = process.env.MONGODB_URI
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+    return client.db(process.env.DB_NAME).collection(collectionName)
+}
+
+
