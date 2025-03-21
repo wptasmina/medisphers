3987e36ca468783c420d18485bda4d7010486948
wptasmina <wptasmina@gmail.com>
2025-03-21 23:35:20 +0600

    added data

--- src/app/_components/FindDoctor.jsx
+++ src/app/_components/FindDoctor.jsx
@@ -6,7 +6,7 @@ export default function FindDoctor() {
     return (
         <div className='w-11/12 mx-auto md:px-4 px-3 my-8'>
             <div className='flex flex-col items-center justify-center '>
-                <h2 className='text-center md:text-5xl text-4xl text-black font-extrabold pb-4'>Find By <span className='text-[#022dbb] '>Doctor's</span></h2>
+                <h2 className='text-center md:text-5xl text-4xl text-black font-extrabold pb-4'>Find By <span className='text-[#022dbb]'>Doctor's</span></h2>
                 <p className='text-center text-md md:w-[800px]'>Find By Doctor's is a feature that allows users to search and filter doctors based on specialization, availability, experience, and other criteria. It helps patients quickly find the right doctor for their needs, view profiles, and book appointments seamlessly. 🚑</p>
             </div>
 
