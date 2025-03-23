479eff00ac2bd2e2e961492ace1da807c24ac26c
wptasmina <wptasmina@gmail.com>
2025-03-23 15:29:14 +0600

    json file delete

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -5,8 +5,8 @@ import { MongoClient } from "mongodb";
 const MONGODB_URI = process.env.MONGODB_URI;
 const MONGODB_DB = process.env.MONGODB_DB;
 
-if (!MONGODB_URI)console.log("Missing MONGODB_URI")
-if (!MONGODB_DB)console.log("Missing MONGODB_DB")
+if (!MONGODB_URI) console.log("Missing MONGODB_URI")
+if (!MONGODB_DB) console.log("Missing MONGODB_DB")
 
 let cachedClient = null;
 let cachedDb = null;
@@ -18,10 +18,10 @@ export const collectionName = {
 export async function connectToDatabase() {
   if (cachedClient && cachedDb) return { client: cachedClient, db: cachedDb };
 
-  const client = await MongoClient.connect(MONGODB_URI);
- 
+  // const client = await MongoClient.connect(MONGODB_URI);
+  const client = new MongoClient(MONGODB_URI);
+  await client.connect();
   const db = client.db(MONGODB_DB);
-
 
   cachedClient = client;
   cachedDb = db;
