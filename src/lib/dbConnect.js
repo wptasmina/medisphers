7e2d298f2101d734f307ca2bdd1f2922c6b123ec
wptasmina <wptasmina@gmail.com>
2025-03-23 13:39:18 +0600

    add data uri

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -3,11 +3,10 @@ import { MongoClient } from "mongodb";
 
 
 const MONGODB_URI = process.env.MONGODB_URI;
-// console.log(process.env.MONGODB_URI); 
 const MONGODB_DB = process.env.MONGODB_DB;
 
-if (!MONGODB_URI) console.log("Missing MONGODB_URI")
-if (!MONGODB_DB) console.log("Missing MONGODB_DB")
+if (!MONGODB_URI)console.log("Missing MONGODB_URI")
+if (!MONGODB_DB)console.log("Missing MONGODB_DB")
 
 let cachedClient = null;
 let cachedDb = null;
@@ -19,11 +18,11 @@ export const collectionName = {
 export async function connectToDatabase() {
   if (cachedClient && cachedDb) return { client: cachedClient, db: cachedDb };
 
-  const client = await MongoClient.connect(MONGODB_URI);
-  // const client = new MongoClient(MONGODB_URI);
-  // await client.connect();
+  // const client = await MongoClient.connect(MONGODB_URI);
+  const client = new MongoClient(process.env.MONGODB_URI);
+  await client.connect();
   const db = client.db(MONGODB_DB);
-  // const db = await client.connect().then(client => client.db(MONGODB_DB));
+
 
   cachedClient = client;
   cachedDb = db;
