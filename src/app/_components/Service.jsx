0fe642d936374be714bea026c3711e4091cee18e
wptasmina <wptasmina@gmail.com>
2025-03-27 05:32:43 +0600

    fixed find search section and top doctors section bg color

--- src/app/_components/Service.jsx
+++ src/app/_components/Service.jsx
@@ -19,14 +19,14 @@ export default async function Service() {
 
   return (
     <>
-      <div className='py-6 dark:bg-gray-950'>
+      <div className='md:py-6 dark:bg-gray-950 py-6'>
         <div className='w-11/12 mx-auto '>
-          <h2 className='md:text-5xl text-4xl text-center font-extrabold mt-8'>Top <span className='text-[#022dbb]'>Doctor's</span></h2>
+          <h2 className='md:text-5xl text-4xl text-center font-extrabold mt-4'>Top <span className='text-[#022dbb]'>Doctor's</span></h2>
 
-          <p className="text-md text-center pt-4 text-gray-500 dark:text-gray-300 font-medium ">
+          <p className="text-md text-center pt-4 pb-4 sm:pb-0 text-gray-500 dark:text-gray-300 font-medium ">
             Top Doctors are expert physicians known for exceptional care and medical excellence.🏆
           </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 sm:my-10">
             {
               doctors.map((item, idx) => (<ServiceCard key={idx} item={item} />))
             }
