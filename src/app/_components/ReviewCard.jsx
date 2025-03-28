089ac69613510b4f498f82be2a3726dbdcc88052
wptasmina <wptasmina@gmail.com>
2025-03-28 13:44:13 +0600

    added review card bg colour and gap fixed

--- src/app/_components/ReviewCard.jsx
+++ src/app/_components/ReviewCard.jsx
@@ -1,19 +1,33 @@
 "use client";
 
+import { Quote } from "lucide-react";
+
 export default function ReviewCard({ review }) {
   if (!review) return null;
   console.log(review);
-  
 
-  const {img, name, role, desc, rating } = review; 
+
+  const { img, name, role, desc, rating } = review;
 
   return (
-    <div className="bg-white dark:bg-gray-900 p-6 rounded-lg shadow-md">
-      <img src={img} alt={name} className="w-20 h-20 rounded-full mb-4" />
-      <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{name}</h3>
-      <p className="text-gray-500 text-sm">{role}</p>
-        <p className="mt-3 text-gray-700 italic dark:text-gray-300">“{desc}”</p>
-      <p className="text-sm text-gray-500 dark:text-gray-300">Rating: {rating}/5</p>
+
+    <div className="bg-white dark:bg-gray-900 p-4 rounded-lg shadow-md mx-4">
+      <p className=" text-gray-500 dark:text-gray-300 font-bold text-4xl">{rating}/5</p>
+      <p className="mt-3 text-gray-700 italic dark:text-gray-300">
+        <span className="inline-block rotate-180 text-lg"><Quote /></span> {/* Rotated opening quote */}
+        {desc}
+        <span className="inline-block"><Quote /></span>
+      </p>
+      <div className="flex justify-between pt-4">
+        <div>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{name}</h3>
+          <p className="text-gray-500 text-sm">{role}</p>
+        </div>
+        <div>
+          <img src={img} alt={name} className="w-16 h-16 rounded-full mb-4" />
+        </div>
+      </div>
     </div>
+
   );
 }
--- src/app/_components/ReviewsClient.jsx
+++ src/app/_components/ReviewsClient.jsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -13,23 +13,59 @@ export default function ReviewsClient({ reviews }) {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    cssEase: "linear"
+    speed: 3000,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+
+    responsive: [
+      {
+        breakpoint: 1624,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 766,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   return (
-    <section className="py-16 bg-gray-50 dark:bg-gray-950 px-6 lg:px-20">
+    <section className="py-16 bg-gray-50 dark:bg-gray-950 px-6 lg:px-20 border">
       <div className="w-11/12 mx-auto text-center">
-        <h2 className="text-3xl font-bold text-gray-800 dark:text-white dark:bg-gray-900 mb-6">
+        <h2 className="text-3xl font-bold text-gray-800 dark:text-white mb-6">
           What People Say About MediSpheres
         </h2>
-        <p className="text-gray-600 dark:text-white">
+        <p className="text-gray-600 dark:text-gray-300">
           Our users love the seamless experience and efficient management solutions.
         </p>
       </div>
 
-      <div className="mt-12 gri grid-cols-1">
+      <div className="mt-12 slider-container">
         <Slider {...settings}>
           {reviews.length === 0 ? (
             <p className="text-center text-gray-600 dark:text-gray-400">
@@ -37,8 +73,10 @@ export default function ReviewsClient({ reviews }) {
             </p>
           ) : (
             reviews.map((review, index) => (
-              <div key={index} className="border p-4 my-2 rounded-md shadow-md">
-                <ReviewCard review={review} />
+              <div key={index} className="px-2">
+                <div className="border rounded-md shadow-md py-4  dark:bg-gray-950">
+                  <ReviewCard review={review} />
+                </div>
               </div>
             ))
           )}
