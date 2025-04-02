4a95834816dfe7dd5a85a496e3e9c82d58ada1cb
wptasmina <wptasmina@gmail.com>
2025-04-02 22:54:35 +0600

    change banner section

---
+++ src/app/_components/Banner.jsx
@@ -0,0 +1,11 @@
+import BannerSlider from './BannerSlider'
+
+export default function Banner() {
+  return (
+    <>
+    <div className='w-11/12 mx-auto'> 
+    <BannerSlider />
+    </div>
+    </>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,17 @@
-import { Button } from "@/components/ui/button"
-import DoctorSection from "@/components/ui/DoctorSection";
 import Hero from "./_components/Hero";
+import Banner from "./_components/Banner";
 import Service from "./_components/Service";
 import Reviews from "./reviews/page";
 import FindDoctor from "./_components/FindDoctor";
+
 
 
 export default function Home() {
   return (
     
    <div className="dark:bg-gray-900 bg-white text-black dark:text-white min-h-screen">
-    <Hero/>
+    {/* <Hero/> */}
+    <Banner />
     <FindDoctor />
     <Service/>
     <Reviews/>
