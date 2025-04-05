3cfa31cacdffcdb1376bcaa6c80aad0681079dd2
wptasmina <wptasmina@gmail.com>
2025-04-06 01:20:07 +0600

    Sign Up page margin fixed

--- src/app/(root)/signup/page.jsx
+++ src/app/(root)/signup/page.jsx
@@ -1,7 +1,7 @@
 import RegistrationForm from "@/components/form/RegistrationForm";
 const page = () => {
   return (
-    <div className="w-full lg:max-w-7xl mx-auto">
+    <div className="w-full mb-20 pt-32 lg:max-w-7xl mx-auto">
       <RegistrationForm />
     </div>
   );
