817857e53390b53345635cda744866f91e9b6320
wptasmina <wptasmina@gmail.com>
2025-04-11 12:28:22 +0600

    dashbord navbar added user icon

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -17,9 +17,14 @@ import {
 } from "@/components/ui/breadcrumb";
 import {
   DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Bell } from "lucide-react";
+
+import { Bell, User } from "lucide-react";
 import { ThemeProvider } from "@/components/shared/theme-provider";
 import { ModeToggle } from "@/components/shared/mode-toggle";
 
@@ -29,58 +34,73 @@ export default function Layout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        {/* Header Section (NavBar) */}
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            {/* Header Section (NavBar) */}
 
-      <header className="flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-        <SidebarTrigger className="-ml-1" />
-        <Separator
-          orientation="vertical"
-          className="mr-2 data-[orientation=vertical]:h-4"
-        />
+            <header className="flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
 
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Current Page</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Current Page</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
 
-        {/* Notifications and Mode Toggle */}
-        <div className="flex items-center ml-auto gap-4">
-          {/* Bell Notification Button */}
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Bell />
-            </DropdownMenuTrigger>
-          </DropdownMenu>
+              {/* Notifications and Mode Toggle */}
+              <div className="flex items-center ml-auto gap-4">
+                {/* Bell Notification Button */}
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <Bell />
+                  </DropdownMenuTrigger>
+                </DropdownMenu>
 
-          {/* Mode Toggle Button */}
-          <ModeToggle />
-        </div>
-      </header>
+                {/* Mode Toggle Button */}
+                <ModeToggle />
 
-      {/* ThemeProvider  */}
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {/* Page Content */}
-        <main>{children}</main>
-      </ThemeProvider>
-    </SidebarInset>
-    </SidebarProvider >
+                {/* User  */}
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="w-10 h-10 flex justify-center items-center cursor-pointer rounded-full transition-all duration-200 border border-gray-600 data-[state=open]:bg-gray-100 dark:data-[state=open]:bg-gray-800">
+                    <User className="w-5 h-5" />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
 
-    </body>
+              </div>
+            </header>
+
+            {/* ThemeProvider  */}
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {/* Page Content */}
+              <main>{children}</main>
+            </ThemeProvider>
+          </SidebarInset>
+        </SidebarProvider >
+
+      </body>
     </html>
   );
 }
