5b7da5b6dba8f088218ffc9f1bbb12ddbaafc747
wptasmina <wptasmina@gmail.com>
2025-04-11 11:46:38 +0600

    Dark and kight mode add

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -20,42 +20,67 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Bell } from "lucide-react";
+import { ThemeProvider } from "@/components/shared/theme-provider";
+import { ModeToggle } from "@/components/shared/mode-toggle";
+
+
 
 export default function Layout({ children }) {
   return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
         {/* Header Section (NavBar) */}
-        <header className="flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <SidebarTrigger className="-ml-1" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Current Page</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
 
-          {/* Notifications */}
+      <header className="flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <SidebarTrigger className="-ml-1" />
+        <Separator
+          orientation="vertical"
+          className="mr-2 data-[orientation=vertical]:h-4"
+        />
+
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Current Page</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
+        {/* Notifications and Mode Toggle */}
+        <div className="flex items-center ml-auto gap-4">
+          {/* Bell Notification Button */}
           <DropdownMenu>
             <DropdownMenuTrigger>
               <Bell />
             </DropdownMenuTrigger>
           </DropdownMenu>
-        </header>
 
+          {/* Mode Toggle Button */}
+          <ModeToggle />
+        </div>
+      </header>
+
+      {/* ThemeProvider  */}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
         {/* Page Content */}
         <main>{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+      </ThemeProvider>
+    </SidebarInset>
+    </SidebarProvider >
+
+    </body>
+    </html>
   );
 }
