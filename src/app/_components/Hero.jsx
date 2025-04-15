23ae7e9a58280173663f504ac69414d6596b85b6
wptasmina <wptasmina@gmail.com>
2025-04-15 20:55:17 +0600

    banner section padding remove

--- src/app/_components/Hero.jsx
+++ src/app/_components/Hero.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 export default function Hero() {
   return (
     <section className='pt-14'>
-      <div className="w-11/12 mx-auto  py-8 px-3 ">
+      <div className="w-11/12 mx-auto  py-8">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 items-center self-start md:gap-8">
           <div>
             <div className="max-w-lg md:max-w-none self-start  px-3 md:px-0">
