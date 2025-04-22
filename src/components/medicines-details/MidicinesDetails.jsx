26fd01171b27032f44085ef4e03c69bdbc7e9971
wptasmina <wptasmina@gmail.com>
2025-04-22 22:43:36 +0600

    added Medicines Suggest componests add to slide

--- src/components/medicines-details/MidicinesDetails.jsx
+++ src/components/medicines-details/MidicinesDetails.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import MedicinesSuggest from '../medicines-suggest/MedicinesSuggest'
 
 export default function MidicinesDetails() {
   return (
@@ -36,8 +37,10 @@ export default function MidicinesDetails() {
           </div>
 
         </div>
-        {/* <div>Task</div> */}
       </div>
+        <div>
+          <MedicinesSuggest/>
+        </div>
     </div>
   )
 }
