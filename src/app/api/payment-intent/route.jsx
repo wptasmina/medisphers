a42f42ca85351c98e2a717d4fc46811299753d3b
wptasmina <wptasmina@gmail.com>
2025-04-28 13:16:51 +0600

    added stripe key

--- src/app/api/payment-intent/route.jsx
+++ src/app/api/payment-intent/route.jsx
@@ -2,10 +2,6 @@
 import Stripe from 'stripe';
 
 
-// const Stripe = require('stripe');
-// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req) {
