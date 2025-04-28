2e32ff7f6f0734d7644c1a143e0582cba7e12a67
anamul hoque <anamul.eee73@gmail.com>
2025-04-28 10:02:25 +0600

    added change

--- src/app/(dashboard)/patient/dashboard/page.jsx
+++ src/app/(dashboard)/patient/dashboard/page.jsx
@@ -19,7 +19,7 @@ const PatientDashboard = () => {
         className="grid gap-6 md:grid-cols-2 lg:grid-cols-3"
       >
         {/* Upcoming Appointments */}
-        <Card className="shadow-lg border-blue-200">
+        <Card className="shadow-lg border-blue-300">
           <CardHeader className="flex items-center gap-3">
             <Calendar className="w-8 h-8 text-[#022dbb]" />
             <CardTitle className="text-lg">Upcoming Appointments</CardTitle>
