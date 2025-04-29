17a91ec7672f0dcd0efecc22a1480dcc23ff49b0
wptasmina <wptasmina@gmail.com>
2025-04-29 14:17:51 +0600

    datails page Available /Unavailable color change

--- src/app/(dashboard)/payment/PaymentPageClient.jsx
+++ src/app/(dashboard)/payment/PaymentPageClient.jsx
@@ -92,7 +92,10 @@ export default function PaymentPageClient() {
           <CheckoutForm clientSecret={clientSecret} bookingData={bookingData} />
         </Elements>
       ) : (
-        <p>Loading payment form...</p>
+        <div className="w-full flex justify-center items-center h-[300px]">
+        <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-[#022dbb] border-solid"> <p>Loading payment form...</p></div>
+      </div>
+       
       )}
     </div>
   );
--- src/app/(dashboard)/payment/page.jsx
+++ src/app/(dashboard)/payment/page.jsx
@@ -4,7 +4,16 @@ import PaymentPageClient from "./PaymentPageClient";
 
 export default function PaymentPage() {
   return (
-    <Suspense fallback={<div>Loading payment page...</div>}>
+    <Suspense
+      fallback={
+        <div className="w-full flex justify-center items-center h-[300px]">
+          <div className="flex flex-col items-center gap-2">
+            <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-[#022dbb] border-solid"></div>
+            <p className="text-[#022dbb] font-semibold">Loading payment form...</p>
+          </div>
+        </div>
+      }
+    >
       <PaymentPageClient />
     </Suspense>
   );
